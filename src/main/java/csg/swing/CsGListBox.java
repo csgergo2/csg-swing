5459a37e52541a70fe27d329d69c14f140458467
csgergo <csgergocs@gmail.com>
2020-10-16 22:37:32 +0200

    added CsGListBox config options

--- src/main/java/csg/swing/CsGListBox.java
+++ src/main/java/csg/swing/CsGListBox.java
@@ -3,26 +3,46 @@ package csg.swing;
 import java.awt.Color;
 import java.lang.reflect.Array;
 import java.util.List;
+import java.util.Optional;
 import java.util.Vector;
 
 import javax.swing.BorderFactory;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionListener;
 
 public class CsGListBox<T> extends JScrollPane {
 
     private int listSelectionModel = ListSelectionModel.SINGLE_SELECTION;
 
-    public CsGListBox(Enum<?> name) {
+    private final ListSelectionListener listSelectionListener;
+
+    public CsGListBox(Enum<?> name, ListSelectionListener listSelectionListener) {
         super();
+        this.listSelectionListener = listSelectionListener;
         setName(name.name());
         setBorder(BorderFactory.createLineBorder(Color.black));
+    }
+
+    public void setListSelectionModel(final int listSelectionModel) {
+        this.listSelectionModel = listSelectionModel;
+    }
+
+    public Optional<T> getSelectedValue() {
+        return getViewport().getView() != null ? Optional.ofNullable(((JList<T>) getViewport().getView()).getSelectedValue()) : Optional.empty();
+    }
+
+    public void setSelectedValue(T value) {
+        ((JList<T>) getViewport().getView()).setSelectedValue(value, true);
     }
 
     public void reset(List<T> items) {
         JList<T> list = new JList<>();
         list.setSelectionMode(listSelectionModel);
+        if (listSelectionListener != null) {
+            list.addListSelectionListener(listSelectionListener);
+        }
         if (items.size() == 0) {
             list.setListData(new Vector<>());
         } else {
@@ -32,6 +52,7 @@ public class CsGListBox<T> extends JScrollPane {
             items.toArray(data);
             list.setListData(data);
             list.setSelectedIndex(items.size() - 1);
+            list.setSelectedIndex(0);
             setViewportView(list);
         }
     }
